1f4593dc9049e24608a71f1465b522c99fd42b69
Starmoon0911 <starmoon9159@gmail.com>
2024-11-11 20:53:56 +0800

    Fix can't create Product

--- src/api/v1/product/createProduct.js
+++ src/api/v1/product/createProduct.js
@@ -2,7 +2,8 @@ const { createProduct } = require('@controller/productController');
 const express = require('express')
 const router = express.Router()
 const ProductUploadHandler = require('../../../multer/product')
-router.post('/create',ProductUploadHandler, createProduct)
+const AvatarUploadHandler = require('../../../multer/avatar')
+router.post('/create', ProductUploadHandler, createProduct)
 module.exports = {
     router,
     name: 'create'
--- src/multer/product.js
+++ src/multer/product.js
@@ -5,15 +5,20 @@ const uploadDir = path.join(__dirname, '../../upload/product');
 
 if (!fs.existsSync(uploadDir)) {
     fs.mkdirSync(uploadDir, { recursive: true });
+    console.log('建立了上傳目錄:', uploadDir);
 }
+
 // 設置 multer 存儲選項
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, path.join(__dirname, 'upload', 'product')); // 設置圖片儲存目錄
+        console.log('設定圖片儲存目錄:', uploadDir);
+        cb(null, uploadDir);  // 設置圖片儲存目錄
     },
     filename: (req, file, cb) => {
         const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-        cb(null, `product-${uniqueSuffix}${path.extname(file.originalname)}`); // 設置圖片的文件名稱
+        const filename = `product-${uniqueSuffix}${path.extname(file.originalname)}`;
+        console.log('設定圖片檔名:', filename);
+        cb(null, filename); // 設置圖片的文件名稱
     }
 });
 
@@ -25,30 +30,32 @@ const upload = multer({
         const filetypes = /jpeg|jpg|png|gif|webp/;
         const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
         const mimetype = filetypes.test(file.mimetype);
-
+ 
         if (mimetype && extname) {
             return cb(null, true);
         }
 
-        // 傳遞錯誤消息，而不是拋出錯誤
         cb('只允許上傳 JPEG、PNG、GIF 或 WEBP 格式的圖片');
     }
 });
 
 // 處理 multer 上傳錯誤
 const ProductUploadHandler = (req, res, next) => {
-    upload.array('images', 5)(req, res, (err) => {  // 可以上傳最多5個檔案
+    upload.array('images', 5)(req, res, (err) => {
         if (err) {
-            // 檢查錯誤類型，並返回用戶友好的錯誤消息
             let message;
             if (err instanceof multer.MulterError) {
-                // 例如文件大小錯誤
                 message = '檔案大小超過限制，請上傳小於5MB的檔案。';
             } else {
-                message = err; // 來自 fileFilter 的錯誤信息
+                message = err;
             }
             return res.status(400).json({ message });
         }
+        
+        req.files = req.files.map(file => ({
+            ...file,
+            path: path.join('/', path.relative(process.cwd(), file.path)) // 加上前面的 '\'
+        }));
         next();
     });
 };
